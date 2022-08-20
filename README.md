ed8c1e5770c53b8d0008f862564a9aa7986e1128
Eyal Pinkas <pinkasey@gmail.com>
2022-08-19 18:02:26 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # cpanel-deploy-action
-deploy to cPanel using cPanel API and git
+This actinos deploys a site to cPanel using the cPanel API,
+by taking the following steps:
+1. Tell cPanel to pull the branch
+2. Tell cPanel to deploy the branch
+3. Poll cPanel deployment status, until the deployment task is complete
+
+To make use of this action, your site should have a valid `.cpanel.yml` file at its root.
+More info about cPanel deployment can be found in the [official documentation](https://docs.cpanel.net/knowledge-base/web-services/guide-to-git-how-to-set-up-deployment/)
+
+
+## Inputs
+
+### `hostname`
+**Required** hostname of cPanel installation, including protocol. for instance: https://hostname.example.com
+
+### `cPanelApiPort`
+Port of cPanel API. The default value is `2083` - don't change it unless you know what you're doing
+
+### `cpanel_token`
+cPanel API token, used for authorization. You should store this as a repository-secret.
+
+### `cpanel_username`
+cPanel username used for API calls. Must be the same username used to create the token. e.g: 'joe'
+
+### `repository_root`
+**Required** folder in which the repository is installed in the target cPanel account. must already exist. e.g: /home/your_account/repositories/your_repository
+
+### `branch`
+**Required** branch to deploy. e.g: 'main' or 'master', or any other branch. Default: `main`
+
+
+#Outputs
+
+### `duration`
+Duration of deployment, in milliseconds
+
+
+## Example usage
+```
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Deploy to cPanel
+        id: deploy
+        uses: pinkasey/cpanel-deploy-action@v1.0.0
+        with:
+          hostname: 'https://hostname.example.com'
+          repository_root: '/home/my_account/repositories/my_repository'
+          branch: main
+          cpanel_token: '${{ secrets.CPANEL_TOKEN }}'
+      - name: echo deploy-duration
+        run: echo "Deployment took ${{ steps.deploy.outputs.duration }} milliseconds"
+```
---
+++ index.js
@@ -0,0 +1,88 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+const main = async () => {
+
+    const timeStart = new Date();
+    try {
+        const maxWaitSeconds = 60 * 5;
+        const hostname = core.getInput('hostname', {required: true});
+        const port = core.getInput('cPanelApiPort', {required: true});
+        const repository_root = core.getInput('repository_root', {required: true});
+        const branch = core.getInput('branch', {required: true});
+        const cpanel_token = core.getInput('cpanel_token', {required: true});
+        const cpanel_username = core.getInput('cpanel_username', {required: true});
+
+        const baseUrl = `${hostname}:${port}:/execute`;
+        console.log(`baseUrl: '${baseUrl}'`);
+        const updateRepoEndpoint = baseUrl + "/VersionControl/update";
+        const createDeploymentTaskEndpoint = baseUrl + "/VersionControlDeployment/create";
+        const getDeploymentStatusEndpoint = baseUrl + "/VersionControlDeployment/retrieve";
+
+        let updateRes = axios.get(updateRepoEndpoint, {
+            params: {
+                repository_root,
+                branch,
+            },
+            headers: {"Authorization": `cpanel ${cpanel_username}:${cpanel_token}`}
+        });
+        console.log(`updateRes: ${updateRes}`);
+        updateRes = updateRes.data;
+        if (updateRes.errors !== null) {
+            // noinspection ExceptionCaughtLocallyJS
+            throw new Error(updateRes.errors);
+        }
+
+        let startDeployRes = axios.get(createDeploymentTaskEndpoint, {
+            params: {
+                repository_root,
+            },
+            headers: {"Authorization": `cpanel ${cpanel_username}:${cpanel_token}`}
+        });
+        console.log(`startDeployRes: ${startDeployRes}`);
+        startDeployRes = startDeployRes.data;
+        if (startDeployRes.errors !== null) {
+            // noinspection ExceptionCaughtLocallyJS
+            throw new Error("Failed to start deployment task: " + JSON.stringify(startDeployRes.errors));
+        }
+        const taskId = startDeployRes.task_id;
+        if (!taskId) {
+            // noinspection ExceptionCaughtLocallyJS
+            throw new Error("Failed to start deployment task - task_id = " + taskId);
+        }
+
+        for (let i=0; i<maxWaitSeconds; i++) {
+            console.log(`polling iteration ${i}`);
+            let pollRes = axios.get(getDeploymentStatusEndpoint, {
+                headers: {"Authorization": `cpanel ${cpanel_username}:${cpanel_token}`}
+            });
+            pollRes = pollRes.data;
+            if (pollRes.errors != null){
+
+            }
+            const taskData = pollRes.data.filter( info => info.task_id === taskId )[0];
+            if (taskData.succeeded != null) {
+                console.log(`task succeeded at ${taskData.succeeded}`);
+                break;
+            }
+            if (taskData.failed != null) {
+                console.log(`task failed at ${taskData.failed}`);
+                console.log(`errors: ${pollRes.errors}`);
+                console.log(`messages: ${pollRes.messages}`);
+                // noinspection ExceptionCaughtLocallyJS
+                throw new Error(`Task failed to deploy. errors: ${pollRes.errors}`);
+            }
+        }
+        
+        const duration = new Date() - timeStart;
+        core.setOutput("duration", duration);
+        console.log(`deployment duration: ${duration}`);
+    } catch (error) {
+        const duration = new Date() - timeStart;
+        console.log(`failed deployment duration: ${duration}`);
+        core.setOutput("duration", duration);
+        core.setFailed(error.message);
+    }
+};
+
+main();
